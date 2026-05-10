cba26945f698daa69e24b42063cc59ab8bd84fd3
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2026-05-10 08:57:24 +0000

    Bump staging images for publisher workspace

--- scripts/infra/build-and-push-images.sh
+++ scripts/infra/build-and-push-images.sh
@@ -2,12 +2,13 @@
 set -eu
 
 REGISTRY="${REGISTRY:-registry.testmedical.ir:5000}"
+TAG="${TAG:-latest}"
 BACKEND_IMAGE="${REGISTRY}/lcp-server"
 FRONTEND_IMAGE="${REGISTRY}/lcp-admin-ui"
 
 export BUILDAH_ISOLATION="${BUILDAH_ISOLATION:-chroot}"
 
-buildah bud -t "${BACKEND_IMAGE}:latest" .
-buildah bud -t "${FRONTEND_IMAGE}:latest" frontend
-buildah push --tls-verify=false "${BACKEND_IMAGE}:latest" "docker://${BACKEND_IMAGE}:latest"
-buildah push --tls-verify=false "${FRONTEND_IMAGE}:latest" "docker://${FRONTEND_IMAGE}:latest"
+buildah bud -t "${BACKEND_IMAGE}:${TAG}" .
+buildah bud -t "${FRONTEND_IMAGE}:${TAG}" frontend
+buildah push --tls-verify=false "${BACKEND_IMAGE}:${TAG}" "docker://${BACKEND_IMAGE}:${TAG}"
+buildah push --tls-verify=false "${FRONTEND_IMAGE}:${TAG}" "docker://${FRONTEND_IMAGE}:${TAG}"
